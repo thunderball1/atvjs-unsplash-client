c566a8be5f5786eaf80d00a8109d49e7492f9324
Adam Vresky <vreskyadam@gmail.com>
2016-03-05 16:04:29 +0100

    menu

--- src/application.js
+++ src/application.js
@@ -3,21 +3,24 @@ import HomePage from './pages/home';
 
 const CLIENT_ID = 'b9288b9e4913497056fbdd1255c0147b6ed3e8e201811f2f3023f6fd5b9e3af0';
 
+
+let Menu = ATV.Menu.create({
+    // any attributes that you want to set on the root level menuBar element of TVML
+    attributes: {},
+    // array of menu item configurations
+    items: [{
+        id: 'home',
+        name: 'Home',
+        page: HomePage,
+        attributes: {
+            autoHighlight: true // auto highlight on navigate
+        }
+    }]
+});
+
 ATV.start({
-	// menu: {
-	// 	attributes: {},
-	// 	items: [{
-	// 		id: 'parade',
-	// 		name: 'Tab 1'
-	// 	}, {
-	// 		id: 'catalog',
-	// 		name: 'Tab 2'
-	// 	}]
-	// },
+	menu: Menu,
 	onLaunch(options) {
-
-    console.log('Launching');
-
-		ATV.Navigation.navigate('home');
+		ATV.Navigation.navigateToMenuPage();
 	}
 });
